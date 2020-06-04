8abc13a32676d6136296a79c7da5dab6b5565504
Кирилл <kirill@MacBook-Air.local>
2020-06-04 11:50:26 +0300

    added create list page and several improvements

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+
+from app.models import User
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('Пользователь', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить?')
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Пользователь', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста, используйте другое имя.')
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
-from flask import render_template, request, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user, login_required
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required, login_user, logout_user
+from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
-from app.models import User
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User, ItemList
 
 
 @app.route('/')
+@app.route('/index')
+@login_required
 def index():
-    return render_template('main_page.html')
+    lists = ItemList.query.filter_by(owner_id=current_user.id).all()
+    return render_template('main_page.html', lists=lists)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -22,20 +26,33 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'GET':
-        return render_template('register.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Поздравляем с регистрацией!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Регистрация', form=form)
 
-
-@app.route('/itemlist')
-def itemlist():
-    return('main app here')
+@app.route('/list')
+def list():
+    return render_template('create_list.html')
